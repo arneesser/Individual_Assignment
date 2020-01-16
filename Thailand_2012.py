a5939cfbe210ff9033652cbd6ddd7ee523d06dc6
Arne E <odenesser@gmail.com>
2020-01-16 12:07:37 +0100

    Extracted the specific keys from first entry

--- Thailand_2012.py
+++ Thailand_2012.py
@@ -1,4 +1,9 @@
 import json
-with open('Thailand_2012.json', encoding = 'utf8') as file:
+with open('Thailand_2012.json', encoding = 'utf16') as file:
     data = json.load(file)
-    
+
+first_dict = data[0]
+
+# Extracting specific keys from the dictionary of first entry
+entry_one = dict((x, first_dict[x]) for x in ['country', 'year', 'best'] if x in first_dict)
+print(entry_one)
