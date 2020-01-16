89b83cae2ee44ef54f98bc48eabf405b2ce5f32f
Arne E <odenesser@gmail.com>
2020-01-16 18:38:56 +0100

    cleaned up some formatting

--- Thailand_2012.py
+++ Thailand_2012.py
@@ -2,13 +2,13 @@ import json
 with open('Thailand_2012.json', encoding = 'utf16') as file:
     data = json.load(file)
 
-entry_one = data[0]
-Headers = ['Country', 'Year', 'Best estimate of deaths']
+
 # Extracting specific keys from the dictionary of first entry
+entry_one = data[0]
 first_dict = dict((x, entry_one[x]) for x in ['country', 'year', 'best'] if x in entry_one)
-small_data = []
 
 # For Loop that creates a list of dictionaries with specified keys
+small_data = []
 for entry in data:
     dictionary = dict((x, entry[x]) for x in ['country', 'year', 'best'] if x in entry)
     small_data.append(dictionary)
@@ -16,6 +16,6 @@ for entry in data:
 
 # Transferring contents to .csv file 
 with open('Thailand_2012.csv', 'w') as file:
-    file.write('Country, Year, Best estimate of deaths\n')
+    file.write('Country, Year, Best \n')
     for line in small_data:
         file.write(f'{line["country"]},{line["year"]},{line["best"]}\n')
