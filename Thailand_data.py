5b705ec4b04b8e81fe054c5b57eae2149e2503d5
Arne E <odenesser@gmail.com>
2020-01-17 10:04:01 +0100

    python file for assignment 2

---
+++ Thailand_data.py
@@ -0,0 +1,19 @@
+import json
+with open('Thailand_data.json', encoding = 'utf16') as file:
+    data = json.load(file)
+
+# Extracting specific keys from the dictionary of first entry
+entry_one = data[0]
+first_dict = dict((x, entry_one[x]) for x in ['country', 'year', 'best'] if x in entry_one)
+
+# For Loop that creates a list of dictionaries with specified keys
+good_data = []
+for entry in data:
+    dictionary = dict((x, entry[x]) for x in ['country', 'year', 'best'] if x in entry)
+    good_data.append(dictionary)
+
+# Transferring contents to .csv file 
+with open('Thailand_data.csv', 'w') as file:
+    file.write('Country, Year, Best \n')
+    for line in good_data:
+        file.write(f'{line["country"]},{line["year"]},{line["best"]}\n')
